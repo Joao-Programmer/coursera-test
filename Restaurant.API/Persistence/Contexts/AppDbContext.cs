b845daed13a2095b8f7bfd2bdfef5cc317451cf4
Joao-Programmer <joao_pr10@hotmail.com>
2021-01-18 13:27:09 -0300

    Andamento da classe AppDbContext - criação da chave estrangeira

--- Restaurant.API/Persistence/Contexts/AppDbContext.cs
+++ Restaurant.API/Persistence/Contexts/AppDbContext.cs
@@ -20,14 +20,32 @@ namespace Restaurant.API.Persistence.Contexts
             builder.Entity<Category>().ToTable("Categories");
             builder.Entity<Category>().HasKey(p => p.id);
             builder.Entity<Category>().Property(p => p.id).IsRequired().ValueGeneratedOnAdd();
-            builder.Entity<Category>().Property(p => p.name).IsRequired().HasMaxLength(30);
+            builder.Entity<Category>().Property(p => p.name).IsRequired().HasMaxLength(100);
+            builder.Entity<Category>().Property(p => p.short_name).IsRequired().HasMaxLength(50);
+            builder.Entity<Category>().Property(p => p.special_instructions).IsRequired().HasMaxLength(4000);
+            builder.Entity<Category>().Property(p => p.url).IsRequired().HasMaxLength(1000);
             builder.Entity<Category>().HasMany(p => p.menu_items).WithOne(p => p.category).HasForeignKey(p => p.categoryId);
 
             builder.Entity<Category>().HasData{
-                new Category{ id = 100, name = "Fruits and Vegetables"},
-                new Category{ id = 101, name = "Dairy"}
+                new Category{ name = "Lunch ", short_name = "L", special_instructions = "Sunday-Friday 11:15am-3:00pm. Served with your choice of rice (Vegetable Fried RIce, Steamed Rice, Brown Rice), AND EITHER soup (Hot & Sour, Wonton, Vegetable, Egg Drop, Chicken Corn Soup) OR veggie egg roll. $1.00 extra to have both soup and egg roll.", url = "aguardando"},
+                new Category{ name = "Soup ", short_name = "A", special_instructions = "", url = "aguardando"}
             };
-            
+
+            builder.Entity<MenuItem>().ToTable("MenuItems");
+            builder.Entity<MenuItem>().HasKey(p => p.id);
+            builder.Entity<MenuItem>().Property(p => p.id).IsRequired().ValueGeneratedOnAdd();
+            builder.Entity<MenuItem>().Property(p => p.description).IsRequired().HasMaxLength(4000);
+            builder.Entity<MenuItem>().Property(p => p.large_portion_name).IsRequired().HasMaxLength(100);
+            builder.Entity<MenuItem>().Property(p => p.name).IsRequired().HasMaxLength(100);
+            builder.Entity<MenuItem>().Property(p => p.price_large).IsRequired();
+            builder.Entity<MenuItem>().Property(p => p.price_small).IsRequired();
+            builder.Entity<MenuItem>().Property(p => p.short_name).IsRequired().HasMaxLength(50);
+            builder.Entity<MenuItem>().Property(p => p.small_portion_name).IsRequired().HasMaxLength(100);
+
+            builder.Entity<Category>()
+                .HasMany(p => p.menu_items)
+                .WithOne(p => p.category)
+                .HasForeignKey(p => p.categoryId);
         }
     }
 }
